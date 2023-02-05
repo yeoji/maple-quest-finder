81a93744ef2fa4546fcd6304d83b2f774f389496
Joey Lee <jq@yeoji.com>
2023-02-05 12:02:19 +1100

    Added GitHub workflow

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
+    redirect: {name: 'Home'}
+  },
+  {
+    path: '/maple-quest-finder/',
     component: () => import('@/layouts/default/Default.vue'),
     children: [
       {
